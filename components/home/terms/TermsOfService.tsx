caafe746e5b5f76452164db5367afde31a330f5c
wangsen3 <wangsen3@360.cn>
2024-11-25 13:06:00 +0800

    'xiugai'

--- components/home/terms/TermsOfService.tsx
+++ components/home/terms/TermsOfService.tsx
@@ -31,7 +31,7 @@ const TermsOfService = () => {
         <li>
           <p className="text-left mt-2 mb-1">Intellectual Property</p>
           <p className="text-left mb-1">
-            The Service and its original content, features, and functionality are owned by Wayne_Dev and are protected by international copyright, trademark, patent, trade secret, and other intellectual property or proprietary rights laws.
+            The Service and its original content, features, and functionality are owned by Mlink and are protected by international copyright, trademark, patent, trade secret, and other intellectual property or proprietary rights laws.
           </p>
         </li>
 
@@ -41,13 +41,22 @@ const TermsOfService = () => {
             We offer various pricing plans as described on our website.
           </p>
           <p className="text-left mb-1">
-            xxxxx
+            Monthly Subscriptions: Eligible for a refund request within 24 hours following the initial purchase. When applying, please provide the reason for your refund request.
+          </p>
+          <p className="text-left mb-1">
+            While we strictly enforce our policies, there may be instances where we are unable to process your refund due to discrepancies in your account. Our goal is to ensure our refund policy is equitable for all users.
+          </p>
+          <p className="text-left mb-1">
+            Should you qualify for a refund, we aim to process it and notify you within 7-10 business days. However, it's important to understand that if you have previously been granted a refund for our services, we reserve the right to refuse additional refund requests. This exclusion is in place to prevent misuse of our refund system and to promote fair usage among our customers.
+          </p>
+          <p className="text-left mb-1">
+            Refund Process: If you believe you are eligible for a refund, please contact us at support@mlink.biz. Our customer service team will review your request and guide you through the refund process.
           </p>
         </li>
         <li>
           <p className="text-left mt-2 mb-1">Limitation of Liability</p>
           <p className="text-left mb-1">
-            To the fullest extent permitted by applicable law, Wayne_Dev and its affiliates shall not be liable for any indirect, incidental, special, consequential, or punitive damages, or any loss of profits or revenues.
+            To the fullest extent permitted by applicable law, Mlink and its affiliates shall not be liable for any indirect, incidental, special, consequential, or punitive damages, or any loss of profits or revenues.
           </p>
         </li>
 
