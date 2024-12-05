2fbc495050930cf558af2db57ad78912ed3f2651
wangsen3 <wangsensnow@163.com>
2024-12-05 13:29:57 +0800

    '修改'

---
+++ pages/api/log-message4.ts
@@ -0,0 +1,64 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+type ResponseData = {
+  message: string
+}
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse<ResponseData>
+) {
+  // import VolcEngineSDK from "volcengine-sdk";
+  const VolcEngineSDK = require("volcengine-sdk");
+  const axios = require("axios");
+
+  const { ApiInfo, ServiceInfo, Credentials, API, Request } = VolcEngineSDK;
+
+  // 设置安全凭证 AK、SK
+  const AK = 'AKLTNTA0M2ViZTdjMmRmNDI2YTg3ODA2ZmEyYzRkMTBhZTE';
+  const SK = 'TWpGalpUTTJOakZoTXpZeU5EZGhaV0psWWpsbVpqRXlZemcxTVdRell6WQ==';
+
+  // 翻译目标语言、翻译文本列表
+  const { message } = request.body
+  const imageBase64 = message;
+
+  // api凭证
+  const credentials = new Credentials(AK, SK, 'translate', 'cn-north-1');
+
+  // 设置请求的 header、query、body
+  const header = new Request.Header({
+    'Content-Type': 'application/json'
+  });
+  const query = new Request.Query({
+    'Action': 'TranslateImage',
+    'Version': '2020-07-01'
+  });
+  const body = new Request.Body({
+    'TargetLanguage': 'zh',
+    'Image': imageBase64
+  });
+
+  // 设置 service、api信息
+  const serviceInfo = new ServiceInfo(
+    'open.volcengineapi.com',
+    header,
+    credentials
+  );
+  const apiInfo = new ApiInfo('POST', '/', query, body);
+
+  // 生成 API
+  const api = API(serviceInfo, apiInfo);
+
+  try {
+    // 等待 axios 请求完成
+    const axiosResponse = await axios.post(api.url, api.params, api.config);
+    const resImage = "data:image/jpeg;base64," + axiosResponse.data.Image;
+
+    // 返回响应
+    response.status(200).json({ message: resImage });
+  } catch (error) {
+    console.error('处理请求时出错:', error);
+    response.status(500).json({ message: '服务器内部错误' });
+  }
+}
+
+
